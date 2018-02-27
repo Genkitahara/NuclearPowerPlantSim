0bb2b5e47d15fb37ab3fb703c62089ef36678415
Genkitahara <gen19990401@gmail.com>
2018-02-27 18:00:15 +0900

    v.0.07
    add information menu and behavior

---
+++ NuclearPowerPlantSim/Assets/Scripts/HideButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideButton : MonoBehaviour {
+    public Transform form;
+    public Animator animator;
+
+    // Use this for initialization
+    void Start () {
+        /*obj = GameObject.Find("unitychan");
+        anim = obj.GetComponent<Animator>();*/
+        animator = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("space"))
+        {
+            animator.SetBool("isHide", !animator.GetBool("isHide"));
+        }
+    }
+    public void Pressd()
+    {
+        animator.SetBool("isHide", !animator.GetBool("isHide"));
+    }
+}
