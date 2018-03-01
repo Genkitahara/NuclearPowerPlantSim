7b85474321bb5c1d40792b21ace5b82dff900a29
Genkitahara <gen19990401@gmail.com>
2018-03-02 03:15:20 +0900

    v.0.11

--- NuclearPowerPlantSim/Assets/Scripts/HideButton.cs
+++ NuclearPowerPlantSim/Assets/Scripts/HideButton.cs
@@ -4,24 +4,29 @@ using UnityEngine;
 
 public class HideButton : MonoBehaviour {
     public Transform form;
-    public Animator animator;
+    public Animator InformationAnimator;
+    public Animator AlertAnimatior;
 
     // Use this for initialization
     void Start () {
         /*obj = GameObject.Find("unitychan");
         anim = obj.GetComponent<Animator>();*/
-        animator = GetComponent<Animator>();
+        //animator = GetComponent<Animator>();
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown("space"))
-        {
-            animator.SetBool("isHide", !animator.GetBool("isHide"));
-        }
+        if (Input.GetKeyDown(KeyCode.LeftShift))InformationAnimator.SetBool("isHide", !InformationAnimator.GetBool("isHide"));
+        if (Input.GetKeyDown(KeyCode.LeftControl)) AlertAnimatior.SetBool("isHide", !AlertAnimatior.GetBool("isHide"));
+
     }
-    public void Pressd()
+    public void InformationPressd()
     {
-        animator.SetBool("isHide", !animator.GetBool("isHide"));
+        InformationAnimator.SetBool("isHide", !InformationAnimator.GetBool("isHide"));
     }
+    public void AlertPressd()
+    {
+        AlertAnimatior.SetBool("isHide", !AlertAnimatior.GetBool("isHide"));
+    }
+
 }
